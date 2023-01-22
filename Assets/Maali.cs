161d1b760d6409be8acc093898497525912d7a62
Tonttu81 <joni.pylkkonen@gmail.com>
2023-01-22 21:06:23 +0200

    Lisätty kerättävä esine ja main menu

--- Assets/Maali.cs
+++ Assets/Maali.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Keräilytavara : MonoBehaviour
+public class Maali : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Koira")
         {
-            Destroy(gameObject);
+            print("voitit");
         }
     }
 }
